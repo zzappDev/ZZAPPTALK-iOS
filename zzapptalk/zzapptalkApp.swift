82790e3ae9c0a61576c5bc17572629594708e6ed
KJH <kjhg5852@naver.com>
2022-03-15 09:18:52 +0900

    Initial Commit

---
+++ zzapptalk/zzapptalkApp.swift
@@ -0,0 +1,17 @@
+//
+//  zzapptalkApp.swift
+//  zzapptalk
+//
+//  Created by KJH on 2022/03/15.
+//
+
+import SwiftUI
+
+@main
+struct zzapptalkApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
